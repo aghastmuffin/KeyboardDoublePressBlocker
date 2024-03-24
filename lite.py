602e9886adfd4de40b3f21516cb44b3a0b8d0dd6
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-03-24 15:26:33 -0700

    Update lite.py

--- lite.py
+++ lite.py
@@ -1,4 +1,5 @@
 import keyboard, time #no installation should be necesary, if it is run pip install keyboard
+#this is not set up in a normal module fashion, however it is on pypi, if you would like to set it up, please configure it yourself.
 while True:
     key = keyboard.read_key()
     if len(key) == 1:
